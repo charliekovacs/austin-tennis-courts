338fabf0024863aa659b2e3c8276dd0cf492b05c
charlie <charlie@charliekovacs.com>
2018-03-15 22:15:36 -0500

    map now plots pre-configured markers and allows users to add their own (does not save yet)

--- js/maps.js
+++ js/maps.js
@@ -1,9 +1,89 @@
 var map;
+var openedInfoWindow = null;
 
+// create the map
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
     zoom: 13,
     center: new google.maps.LatLng(30.2946084, -97.7218932),
     mapTypeId: 'roadmap'
   });
+
+  var script = document.createElement('script');
+  script.src = 'markers.js';
+  document.getElementsByTagName('head')[0].appendChild(script);
+
+
+  google.maps.event.addListener(map, 'click', function(event) {
+    placeMarker(event.latLng);
+  });
+
+
+
+}
+
+// drop in the pre-existing markers
+window.feedCallback = function(results) {
+  var infowindow = new google.maps.InfoWindow();
+
+  for (var i = 0; i < results.features.length; i++) {
+    var coordinates = results.features[i].geometry.coordinates;
+    var latLng = new google.maps.LatLng(coordinates[0], coordinates[1]);
+
+    var marker = new google.maps.Marker({
+      position: latLng,
+      map: map,
+      animation: google.maps.Animation.DROP,
+      optimized: false,
+      info: results.features[i].properties.notes
+    });
+
+    google.maps.event.addListener(marker, 'click', function() {
+        infowindow.setContent(this.info);
+
+        if (openedInfoWindow != null) openedInfoWindow.close();
+        infowindow.open(map, this);
+        openedInfoWindow = infowindow;
+    });
+  }
+}
+
+// create new markers (when user clicks on map)
+function placeMarker(location) {
+
+  var marker = new google.maps.Marker({
+    position: location,
+    map: map,
+    draggable: true,
+    optimized: false,
+    content: '<div id="form"><table><tr><td>Notes:</td><td><input type="text" id="notes"/></td></tr><tr><td><input type="button" value="Submit" onclick="saveData(document.getElementById(\'notes\').value)"/></td></tr></table></div>'
+  });
+
+  marker.setIcon('http://maps.google.com/mapfiles/ms/icons/blue-dot.png');
+
+  var infowindow = new google.maps.InfoWindow();
+
+  google.maps.event.addListener(marker, 'click', function() {
+    infowindow.setContent(this.content);
+
+    if (openedInfoWindow != null) openedInfoWindow.close();
+    infowindow.open(map, this);
+    openedInfoWindow = infowindow;
+  });
+
+  google.maps.event.addListener(marker, 'dblclick', function() {
+    deleteMarker(this);
+  });
+
+}
+
+// delete markers (on double click)
+function deleteMarker(marker) {
+  marker.setMap(null);
+}
+
+function saveData(notes) {
+  alert(notes);
+  openedInfoWindow.close();
+  openedInfoWindow = null;
 }
---
+++ markers.js
@@ -0,0 +1,111 @@
+feedCallback(
+{
+	"type":"FeatureCollection",
+	"features":
+	[
+		{
+		  "type":"Feature",
+		  "properties":
+		  {
+			"notes":"<a id=\"button\" href=\"#\">Click Me</a>"
+		  },
+		  "geometry":
+		  {
+		    "type":"Point",
+		    "coordinates":[30.236968, -97.788188]
+		  }
+		},
+
+		{
+		  "type":"Feature",
+		  "properties":
+		  {
+			"notes":"Public</br>Not lit</br><a href=\"#\" onclick=\"myClick()\">Submit new court</a>"
+		  },
+		  "geometry":
+		  {
+		    "type":"Point",
+		    "coordinates":[30.231731, -97.753826]
+		  }
+		},
+
+		{
+		  "type":"Feature",
+		  "properties":
+		  {
+			"notes":"Public</br>Not lit</br><a href=\"https://google.com\">Submit new court</a>"
+		  },
+		  "geometry":
+		  {
+		    "type":"Point",
+		    "coordinates":[30.232409, -97.744658]
+		  }
+		},
+
+		{
+		  "type":"Feature",
+		  "properties":
+		  {
+			"notes":"Public</br>Not lit</br><a href=\"https://google.com\">Submit new court</a>"
+		  },
+		  "geometry":
+		  {
+		    "type":"Point",
+		    "coordinates":[30.247784, -97.743138]
+		  }
+		},
+
+		{
+		  "type":"Feature",
+		  "properties":
+		  {
+			"notes":"Public</br>Not lit</br><a href=\"https://google.com\">Submit new court</a>"
+		  },
+		  "geometry":
+		  {
+		    "type":"Point",
+		    "coordinates":[30.252050, -97.729865]
+		  }
+		},
+
+		{
+		  "type":"Feature",
+		  "properties":
+		  {
+			"notes":"Public</br>Not lit</br><a href=\"https://google.com\">Submit new court</a>"
+		  },
+		  "geometry":
+		  {
+		    "type":"Point",
+		    "coordinates":[30.265945, -97.720923]
+		  }
+		},
+
+		{
+		  "type":"Feature",
+		  "properties":
+		  {
+			"notes":"Public</br>Not lit</br><a href=\"https://google.com\">Submit new court</a>"
+		  },
+		  "geometry":
+		  {
+		    "type":"Point",
+		    "coordinates":[30.270391, -97.722255]
+		  }
+		},
+
+		{
+		  "type":"Feature",
+		  "properties":
+		  {
+			"notes":"Public</br>Not lit</br><a href=\"https://google.com\">Submit new court</a>"
+		  },
+		  "geometry":
+		  {
+		    "type":"Point",
+		    "coordinates":[30.270862, -97.713924]
+		  }
+		}
+	]
+}
+);
